0587d0053c44459fe250d0aca2897da803ea7b15
Trần Văn Linh <133183567+LINHTRAN9x@users.noreply.github.com>
2023-10-14 17:30:48 +0700

    Add files via upload

---
+++ css/store.js
@@ -0,0 +1,38 @@
+$(window).on('load', function() {
+    $('body').animate({
+        opacity: 1
+    },200)});
+
+
+$('.btn-pop-up').on('click', function() {
+    $('.pop-up-container').addClass('show');
+});
+
+$('.reset-tracking').on('click', function(event) {
+    event.stopPropagation(); // Ngăn chặn sự kiện click truyền lên đến .pop-up-container
+    $('.pop-up-container').removeClass('show');
+});
+
+$('.closei').on('click', function(event) {
+    event.stopPropagation(); // Ngăn chặn sự kiện click truyền lên đến .pop-up-container
+    $('.pop-up-container').removeClass('show');
+});
+
+$(function() {
+    const sideMenu = $('.slide-menu-container');
+    const menuBtn = $('#menu-btn');
+    const closeBtn = $('#close-btn');
+
+  
+    menuBtn.on('click', function() {
+      sideMenu.css('display', 'block');
+    });
+  
+    closeBtn.on('click', function() {
+      sideMenu.css('display', 'none');
+    });
+
+});
+
+
+
